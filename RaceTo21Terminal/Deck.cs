fad4a9c775077ec5aba9b454d112d97959e58704
ColdEgg-T T <100256365+Valertina331@users.noreply.github.com>
2024-02-13 14:17:10 -0500

    Update Deck.cs

--- RaceTo21Terminal/Deck.cs
+++ RaceTo21Terminal/Deck.cs
@@ -1,4 +1,4 @@
-﻿using System;
+    ﻿using System;
 using System.Collections.Generic;
 using System.Linq; // currently only needed if we use alternate shuffle method
 
@@ -6,7 +6,7 @@ namespace RaceTo21
 {
     public class Deck
     {
-        List<string> cards = new List<string>();
+        List<Card> cards = new List<Card>();
 
         public Deck()
         {
@@ -18,25 +18,47 @@ namespace RaceTo21
                 foreach (string cardSuit in suits)
                 {
                     string cardName;
+                    string cardLongName;
+                    
                     switch (cardVal)
                     {
                         case 1:
                             cardName = "A";
+                            cardLongName = "Ace of ";
                             break;
                         case 11:
                             cardName = "J";
+                            cardLongName = "Jack of ";
                             break;
                         case 12:
                             cardName = "Q";
+                            cardLongName = "Queen of ";
                             break;
                         case 13:
                             cardName = "K";
+                            cardLongName = "King of ";
                             break;
                         default:
                             cardName = cardVal.ToString();
+                            cardLongName = cardVal.ToString() + " of ";
                             break;
                     }
-                    cards.Add(cardName + cardSuit);
+                    switch (cardSuit)
+                    {
+                        case "S":
+                            cardLongName += "Spades";
+                            break;
+                        case "H":
+                            cardLongName += "Hearts";
+                            break;
+                        case "C":
+                            cardLongName += "Clubs";
+                            break;
+                        case "D":
+                            cardLongName += "Diamonds";
+                            break;
+                    }
+                    cards.Add(new Card {ID = cardName + cardSuit, name = cardLongName});
                 }
             }
         }
@@ -52,9 +74,9 @@ namespace RaceTo21
 
             // multi-line method that uses Array notation on a list!
             // (this should be easier to understand)
-            for (int i=0; i<cards.Count; i++)
+            for (int i = 0; i < cards.Count; i++)
             {
-                string tmp = cards[i];
+                Card tmp = cards[i];
                 int swapindex = rng.Next(cards.Count);
                 cards[i] = cards[swapindex];
                 cards[swapindex] = tmp;
@@ -69,9 +91,9 @@ namespace RaceTo21
 
         public void ShowAllCards()
         {
-            for (int i=0; i<cards.Count; i++)
+            for (int i = 0; i < cards.Count; i++)
             {
-                Console.Write(i+":"+cards[i]); // a list property can look like an Array!
+                Console.Write((i + 1) + ":" + cards[i]); // a list property can look like an Array!
                 if (i < cards.Count -1)
                 {
                     Console.Write(" ");
@@ -84,7 +106,7 @@ namespace RaceTo21
 
         public string DealTopCard()
         {
-            string card = cards[cards.Count - 1];
+            Card card = cards[cards.Count - 1];
             cards.RemoveAt(cards.Count - 1);
             // Console.WriteLine("I'm giving you " + card);
             return card;
